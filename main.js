3f0bf42fa0fcd38f57e30f4b710f1315ec5c7632
Riccardo Murru <riccardo.mr@gmail.com>
2020-04-28 15:56:24 +0200

    add. ajax request + handlebars template

--- main.js
+++ main.js
@@ -7,5 +7,40 @@
  */
 
 $(document).ready(function() {
-	//Code
-});
+	
+	var cdContainer = $('.cds-container');
+	// Handlebars init
+	var source = $('#disc-template').html();
+	var template = Handlebars.compile(source);
+
+	// Ajax request
+	$.ajax({
+		url: 'https://flynn.boolean.careers/exercises/api/array/music',
+		method: 'GET',
+		success: function(result) {
+
+			if (result.success) {
+				for (var i = 0; i < result.response.length; i++) {
+					var item = result.response[i];
+					var context = {
+						cover: item.poster,
+						title: item.title,
+						author: item.author,
+						year: item.year
+					}
+
+					var output = template(context);
+					cdContainer.append(output)
+				};
+
+			}
+		},
+		error: function() {
+			console.log('Error');
+			
+		}
+	});
+
+
+}); // end doc ready
+
